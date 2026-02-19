9bd7a84833be4cf0d5c212a800fa082915784b8f
Akash <1999akashpanda@gmail.com>
2026-02-19 18:30:15 +0530

    Date - 19

--- practice.js
+++ practice.js
@@ -385,3 +385,29 @@ function findSecondLargest(arr){
 //   };
 //   console.log(row);
 // };
+
+// for(let i = 0; i <= 5; i++){
+//   let row = "";
+//   for(let j = 1; j < i + 1; j++){
+//     row = row +" "+ j;
+//   };
+//   console.log(row);
+// };
+
+for(let i = 0; i < 5; i++){
+  let row = "";
+  for(let j = 0; j <= i; j++){
+    row = row + " " + (i + 1);
+  }
+  console.log(row);
+  
+}
+
+for(let i = 1; i <= 5; i++){
+  let row = "";
+  for(let j = 1; j <= i; j++){
+    row = row + (i);
+  };
+  console.log(row);
+  
+}
