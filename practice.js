ce7e9104ec4059ca50155a337439f74324f7a909
Akash <1999akashpanda@gmail.com>
2026-02-22 09:57:00 +0530

    Date-22

--- practice.js
+++ practice.js
@@ -428,13 +428,40 @@ function findSecondLargest(arr){
 //   console.log(row);
 // };
 
-for(let i = 0; i < 5; i++){
-  let row = "";
-  for(let j = 0; j < 5-(i+1); j++){
-    row = row + " -";
-  };
-  for(let k = 0; k < i + 1; k++){
-    row = row + " *"
+// for(let i = 0; i < 5; i++){
+//   let row = "";
+//   for(let j = 0; j < 5-(i+1); j++){
+//     row = row + " -";
+//   };
+//   for(let k = 0; k < i + 1; k++){
+//     row = row + " *"
+//   };
+//   console.log(row);
+// }
+
+let toggle = 1;
+for(let i = 0; i<5; i++){
+  let row = ""; 
+  for(let j = 0; j < i + 1; j++){
+    row = row + toggle;
+    if(toggle === 1){
+      toggle = 0;
+    } else {
+      toggle = 1;
+    }
   };
   console.log(row);
 }
+
+for(let i = 0; i< 5; i++){
+  let row = "", toggle = 1;
+  for(let j = 0; j < i +1; j++){
+    row = row + toggle;
+    if(toggle === 1){
+      toggle = 0;
+    }else {
+      toggle = 1;
+    };
+  };
+  console.log(row);
+};
